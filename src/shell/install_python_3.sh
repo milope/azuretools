7bfa65c5ab3a382cdae6948723c15b9915ce56d2
Michael Lopez <milope@microsoft.com>
2020-05-01 11:21:27 -0500

    Update install_python_3.sh

--- src/shell/install_python_3.sh
+++ src/shell/install_python_3.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-sudo apt install python3 -f
-sudo apt install python3-pip -f
+sudo apt-get update -y
+sudo apt install python3 -q --yes
+sudo apt install python3-pip -q --yes
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
 #sudo update-alternatives --config python3
-sudo pip3 install azure-eventhub --pre
+sudo apt autoremove
+sudo pip3 install azure-eventhub --pre -q
