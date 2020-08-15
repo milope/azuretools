8447a85b456d988d69e6d98ca6c2cfff0f5ff083
Michael Lopez <milope@microsoft.com>
2020-08-15 11:29:46 -0500

    Update install_python_3.sh
    Commenting out a particular line.

--- src/shell/install_python_3.sh
+++ src/shell/install_python_3.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt install python3 -q --yes
 sudo apt install python3-pip -q --yes
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
-sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
+#sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 2
 #sudo update-alternatives --config python3
 sudo apt autoremove --yes
 sudo pip3 install azure-eventhub --pre -q
