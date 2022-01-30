c2b861de0b6f94f3097dbafa23ecb14344de0c36
Michael Lopez <milope@microsoft.com>
2022-01-29 18:35:45 -0600

    Corrected Get-PolicyStates.ps1 to iterate correcty

--- src/powershell/policy/Get-PolicyStates.ps1
+++ src/powershell/policy/Get-PolicyStates.ps1
@@ -33,11 +33,12 @@ if($null -eq $context -or $null -eq $context.Account) {
 $subscriptionId = $context.Subscription.Id
 $extra =  $(if($null -ne $filter -and -not [String]::IsNullOrEmpty($filter.Trim())) { "&`$filter=$($filter.Trim())" } else { [String]::Empty })
 $apiVersion = "2019-10-01"
-$url = "/subscriptions/$($subscriptionId)/providers/Microsoft.PolicyInsights/policyStates/latest/queryResults?api-version=$($apiVersion)$($extra)"
+$managementEndpoint = (Get-AzContext).Environment.ResourceManagerUrl
+$url = "$($managementEndpoint)subscriptions/$($subscriptionId)/providers/Microsoft.PolicyInsights/policyStates/latest/queryResults?api-version=$($apiVersion)$($extra)"
 $method = "POST"
 
 do {
-    $data = Invoke-AzRestMethod -Path $url -Method $method
+    $data = Invoke-AzRestMethod -Uri $url -Method $method
     $data = ($data.Content | ConvertFrom-Json)
 
     $data.value | Select-Object -Property resourceId, policyDefinitionName, isCompliant
