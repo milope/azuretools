d24f8bfe07e39eec8fc0c796cbf38a48e033fd5c
Michael Lopez <milope@microsoft.com>
2022-05-25 01:01:09 -0500

    Added Storage Upload to captures

--- src/powershell/debugging/Start-NetworkCapture.ps1
+++ src/powershell/debugging/Start-NetworkCapture.ps1
@@ -36,6 +36,12 @@ Specify where to create the file.
 
 Specify the max size in megabytes.
 
+.PARAMETER StorageContainerSASUrl
+
+Specify a Storage Contaienr SAS URL to upload a file.
+Navigate to the Container and generate a SAS with
+Write, Create type of permissions.
+
 .PARAMETER OnlyConvertFile
 
 Only convert the ETL trace to WireShark pcapng
@@ -51,6 +57,7 @@ function Start-NetworkCapture {
         [String][Parameter(Mandatory=$true)]$TraceFile,
         [UInt32][Parameter(Mandatory=$false)]$MaxSize=512,
         [UInt32][Parameter(Mandatory=$false)]$MaxTimeInSeconds,
+        [String][Parameter(Mandatory=$false)]$StorageContainerSASUrl,
         [Switch]$OnlyConvertFile
     )
     begin {
@@ -80,23 +87,98 @@ function Start-NetworkCapture {
             netsh trace stop
         }
 
+        $etl2pcapnglocation = Join-Path -Path $traceDirectory -ChildPath "etl2pcapng\x64\etl2pcapng.exe"
+        $pcapngPath = Join-Path -Path $traceDirectory -ChildPath ([String]::Concat([System.IO.Path]::GetFileNameWithoutExtension($TraceFile), ".pcapng"))
         if(Test-Path -Path $TraceFile) {
             $temp = $ProgressPreference
             try {
                 $ProgressPreference = "SilentlyContinue"
-                $etl2pcapnglocation = Join-Path -Path $traceDirectory -ChildPath "etl2pcapng\x64\etl2pcapng.exe"
                 if(-not (Test-Path $etl2pcapnglocation)) {
+                    Write-Host "[$([DateTime]::UtcNow.ToString("o"))] Downloading etl2pcapng.zip from repository."
                     $zipFile = (Join-Path $traceDirectory "etl2pcapng.zip")
                     Invoke-WebRequest -UseBasicParsing -Uri "https://github.com/microsoft/etl2pcapng/releases/download/v1.7.0/etl2pcapng.zip" -OutFile $zipFile
+                    Write-Host "[$([DateTime]::UtcNow.ToString("o"))] Extracting etl2pcapng.zip."
                     Expand-Archive -Path $zipFile -DestinationPath $traceDirectory
-                    $pcapngPath = Join-Path -Path $traceDirectory -ChildPath ([String]::Concat([System.IO.Path]::GetFileNameWithoutExtension($TraceFile), ".pcapng"))
-                }                
+                }
+                
+                Write-Host "[$([DateTime]::UtcNow.ToString("o"))] Converting capture to WireShark pcapng format."
                 &$etl2pcapnglocation $TraceFile $pcapngPath
             }
             finally {
                 $ProgressPreference = $temp
             }
         }
+
+        if($PSBoundParameters["StorageContainerSASUrl"]) {
+            [System.Uri]$sas = [System.Uri]::new("http://localhost")
+            try {
+                $sas = [System.Uri]::new($StorageContainerSASUrl)
+            }
+            catch {
+                Write-Warning "[$([DateTime]::UtcNow.ToString("o"))] SAS URL is not a valid URL syntactically. Will not upload to storage."
+            }
+
+            if($sas.Segments.Length -ne 2) {
+                Write-Warning "[$([DateTime]::UtcNow.ToString("o"))] SAS URL is not a Container SAS URL. Please generate a contaienr SAS URL. Will not upload to storage."
+                Write-Warning "[$([DateTime]::UtcNow.ToString("o"))] Below is an example."
+                Write-Warning "[$([DateTime]::UtcNow.ToString("o"))] https://storage.blob.core.windows.net/container?sp=cw&st=2022-05-25T05:31:23Z&se=2022-05-25T13:31:23Z&spr=https&sv=2020-08-04&sr=c&sig=******"
+            }
+
+            if(Test-Path $TraceFile) {
+                $traceFileName = [System.IO.Path]::GetFileName($TraceFile)
+                Write-Host "[$([DateTime]::UtcNow.ToString("o"))] Uploading $traceFileName to specified Azure Storage."
+                $newSAS = "$($sas.Scheme)://$($sas.Host)/$($sas.Segments[1])/$($traceFileName)$($sas.Query)"
+                try {
+                    Invoke-WebRequest -Uri $newSAS -UseBasicParsing -Method Put -InFile $TraceFile `
+                        -Headers @{
+                            Date=[DateTime]::UtcNow.ToString("s")
+                            "x-ms-version"="2015-02-21"
+                            "x-ms-blob-type"="BlockBlob"
+                        } | Out-Null
+                }
+                catch {
+                    Write-Warning "[$([DateTime]::UtcNow.ToString("o"))] Could not upload $traceFileName to specified Azure Storage."
+                }
+            }
+
+            if(Test-Path $pcapngPath ) {
+                $traceFileName = [System.IO.Path]::GetFileName($pcapngPath)
+                Write-Host "[$([DateTime]::UtcNow.ToString("o"))] Uploading $traceFileName to specified Azure Storage."
+                $newSAS = "$($sas.Scheme)://$($sas.Host)/$($sas.Segments[1])/$($traceFileName)$($sas.Query)"
+                try {
+                    Invoke-WebRequest -Uri $newSAS -UseBasicParsing -Method Put -InFile $pcapngPath `
+                        -Headers @{
+                            Date=[DateTime]::UtcNow.ToString("s")
+                            "x-ms-version"="2015-02-21"
+                            "x-ms-blob-type"="BlockBlob"
+                        } | Out-Null
+                }
+                catch {
+                    Write-Warning "[$([DateTime]::UtcNow.ToString("o"))] Could not upload $traceFileName to specified Azure Storage."
+                }
+            }
+
+            $cabFile = Join-Path -Path (Split-Path $traceFile) -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension($traceFile) + ".cab")
+
+            if(Test-Path $cabFile ) {
+                $traceFileName = [System.IO.Path]::GetFileName($cabFile)
+                Write-Host "[$([DateTime]::UtcNow.ToString("o"))] Uploading $traceFileName to specified Azure Storage."
+                $newSAS = "$($sas.Scheme)://$($sas.Host)/$($sas.Segments[1])/$($traceFileName)$($sas.Query)"
+                try {
+                    Invoke-WebRequest -Uri $newSAS -UseBasicParsing -Method Put -InFile $cabFile `
+                        -Headers @{
+                            Date=[DateTime]::UtcNow.ToString("s")
+                            "x-ms-version"="2015-02-21"
+                            "x-ms-blob-type"="BlockBlob"
+                        } | Out-Null
+                }
+                catch {
+                    Write-Warning "[$([DateTime]::UtcNow.ToString("o"))] Could not upload $traceFileName to specified Azure Storage."
+                }
+            }
+        }
+
+        Write-Host "[$([DateTime]::UtcNow.ToString("o"))] Operation completed."
     }
     process {
 
