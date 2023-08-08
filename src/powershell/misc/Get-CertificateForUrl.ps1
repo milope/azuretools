a6f04d798112554e68dddca24f49e29eb9b47575
Michael Lopez <milope@microsoft.com>
2023-08-08 18:40:02 -0500

    The Get-CertificateForUrl.ps1 module had a few bugs. Corrected a bug where the SNI was wrong, corrected a bug where all certificates were determined as invalid based on a warning.

--- src/powershell/misc/Get-CertificateForUrl.ps1
+++ src/powershell/misc/Get-CertificateForUrl.ps1
@@ -34,6 +34,7 @@ This cmdlet will return the result from calling Get-Item on the recently downloa
 #>
 function Get-CertificateForUrl
 {
+    [CmdletBinding()]
     param (
         [String][Parameter(Mandatory=$true)]$URL,
         [String][Parameter(Mandatory=$true)]$FilePath
@@ -48,21 +49,72 @@ function Get-CertificateForUrl
         $port = $uri.Port
 
 
-        $tcpClient = New-Object System.Net.Sockets.TcpClient
+        $tcpClient = [System.Net.Sockets.TcpClient]::new()
         try {
             $tcpClient.Connect($host2, $port)
 
-            $ssl = New-Object System.Net.Security.SslStream $tcpClient.GetStream(), $false, {
-                Write-Warning "NOTE: The server certificate is invalid. This script is ignoring the error."
+            $ssl = [System.Net.Security.SslStream]::new($tcpClient.GetStream(), $false, {
+                [CmdletBinding()]
+                param
+                (
+                    [Object]$sndr,
+                    [Security.Cryptography.X509Certificates.X509Certificate]$certificate,
+                    [Security.Cryptography.X509Certificates.X509Chain]$chain,
+                    [Net.Security.SslPolicyErrors]$sslPolicyError
+                )
+                
+                if($sslPolicyError -eq [Net.Security.SslPolicyErrors]::None) {
+                    $true
+                    return
+                }
+                Write-Warning "NOTE: The certificate is invalid according to validation procedure. This script is ignoring the error:"
+                if(($sslPolicyError -band [Net.Security.SslPolicyErrors]::RemoteCertificateNameMismatch) -eq [Net.Security.SslPolicyErrors]::RemoteCertificateNameMismatch) {
+                    Write-Warning "-- Remote certificate name mismatch."
+                }
+                if(($sslPolicyError -band [Net.Security.SslPolicyErrors]::RemoteCertificateChainErrors) -eq [Net.Security.SslPolicyErrors]::RemoteCertificateChainErrors) {
+                    Write-Warning "-- Remote certificate is not trusted. Chain errors below"
+                    $chain.ChainStatus | ForEach-Object {
+                        switch($_) {
+                            [System.Security.Cryptography.X509Certificates]::CtlNotSignatureValid { "-- Specifies that the certificate trust list (CTL) contains an invalid signature."}
+                            [System.Security.Cryptography.X509Certificates]::CtlNotTimeValid { "-- Specifies that the certificate trust list (CTL) is not valid because of an invalid time value, such as one that indicates that the CTL has expired."}
+                            [System.Security.Cryptography.X509Certificates]::CtlNotValidForUsage { "-- Specifies that the certificate trust list (CTL) is not valid for this use."}
+                            [System.Security.Cryptography.X509Certificates]::Cyclic { "-- Specifies that the X509 chain could not be built."}
+                            [System.Security.Cryptography.X509Certificates]::ExplicitDistrust { "-- Specifies that the certificate is explicitly distrusted."}
+                            [System.Security.Cryptography.X509Certificates]::HasExcludedNameConstraint { "-- Specifies that the X509 chain is invalid because a certificate has excluded a name constraint."}
+                            [System.Security.Cryptography.X509Certificates]::HasNotDefinedNameConstraint { "-- Specifies that the certificate has an undefined name constraint"}
+                            [System.Security.Cryptography.X509Certificates]::HasNotPermittedNameConstraint { "-- Specifies that the certificate has an impermissible name constraint."}
+                            [System.Security.Cryptography.X509Certificates]::HasNotSupportedCriticalExtension { "-- Specifies that the certificate does not support a critical extension."}
+                            [System.Security.Cryptography.X509Certificates]::HasNotSupportedNameConstraint { "-- Specifies that the certificate does not have a supported name constraint or has a name constraint that is unsupported."}
+                            [System.Security.Cryptography.X509Certificates]::HasWeakSignature { "-- Specifies that the certificate has not been strong signed. Typically, this indicates that the MD2 or MD5 hashing algorithms were used to create a hash of the certificate."}
+                            [System.Security.Cryptography.X509Certificates]::InvalidBasicConstraints { "-- Specifies that the X509 chain is invalid due to invalid basic constraints."}
+                            [System.Security.Cryptography.X509Certificates]::InvalidExtension { "-- Specifies that the X509 chain is invalid due to an invalid extension."}
+                            [System.Security.Cryptography.X509Certificates]::InvalidNameConstraints { "-- Specifies that the X509 chain is invalid due to invalid name constraints."}
+                            [System.Security.Cryptography.X509Certificates]::InvalidPolicyConstraints { "-- Specifies that the X509 chain is invalid due to invalid policy constraints."}
+                            [System.Security.Cryptography.X509Certificates]::NoError { "-- Specifies that the X509 chain has no errors."}
+                            [System.Security.Cryptography.X509Certificates]::NoIssuanceChainPolicy { "-- Specifies that there is no certificate policy extension in the certificate. This error would occur if a group policy has specified that all certificates must have a certificate policy."}
+                            [System.Security.Cryptography.X509Certificates]::NotSignatureValid { "-- Specifies that the X509 chain is invalid due to an invalid certificate signature."}
+                            [System.Security.Cryptography.X509Certificates]::NotTimeNested { "-- Deprecated. Specifies that the CA (certificate authority) certificate and the issued certificate have validity periods that are not nested. For example, the CA cert can be valid from January 1 to December 1 and the issued certificate from January 2 to December 2, which would mean the validity periods are not nested."}
+                            [System.Security.Cryptography.X509Certificates]::NotTimeValid { "-- Specifies that the X509 chain is not valid due to an invalid time value, such as a value that indicates an expired certificate."}
+                            [System.Security.Cryptography.X509Certificates]::NotValidForUsage { "-- Specifies that the key usage is not valid."}
+                            [System.Security.Cryptography.X509Certificates]::OfflineRevocation { "-- Specifies that the online certificate revocation list (CRL) the X509 chain relies on is currently offline."}
+                            [System.Security.Cryptography.X509Certificates]::PartialChain { "-- Specifies that the X509 chain could not be built up to the root certificate."}
+                            [System.Security.Cryptography.X509Certificates]::RevocationStatusUnknown { "-- Specifies that it is not possible to determine whether the certificate has been revoked. This can be due to the certificate revocation list (CRL) being offline or unavailable."}
+                            [System.Security.Cryptography.X509Certificates]::Revoked { "-- Specifies that the X509 chain is invalid due to a revoked certificate."}
+                            [System.Security.Cryptography.X509Certificates]::UntrustedRoot { "-- Specifies that the X509 chain is invalid due to an untrusted root certificate."}
+                        }
+                    }
+                }
+                if(($sslPolicyError -band [Net.Security.SslPolicyErrors]::RemoteCertificateNotAvailable) -eq [Net.Security.SslPolicyErrors]::RemoteCertificateNotAvailable) {
+                    Write-Warning "-- Remote certificate was not presented."
+                }
                 $true
-            }, $null
+            })
 
             try {
-                $certificates = New-Object System.Security.Cryptography.X509Certificates.X509CertificateCollection
-                $enabledProtocols = [System.Security.Authentication.SslProtocols]::Tls11
-
-                $ssl.AuthenticateAsClient($host, $certificates, $enabledProtocols, $false)
-                $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $ssl.RemoteCertificate
+                $certificates = [System.Security.Cryptography.X509Certificates.X509CertificateCollection]::new()
+                $enabledProtocols = [System.Security.Authentication.SslProtocols]::Tls12
+                $ssl.AuthenticateAsClient($host2, $certificates, $enabledProtocols, $false)
+                $cert = [System.Security.Cryptography.X509Certificates.X509Certificate2]::new($ssl.RemoteCertificate)
                 $contentType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
                 $certBytes = $cert.Export($contentType)
                 [IO.File]::WriteAllBytes($FilePath, $certBytes)
